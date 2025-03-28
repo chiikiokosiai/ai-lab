396412862d344527ad0dc34c956cbefac5f2b134
jojoflo2022 <ultrarockdaffunda@gmail.com>
2025-03-28 16:27:53 +0900

    Update files

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
+// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// GitHub Pagesにデプロイする場合は、baseを設定する
-// 独自ドメインを使う場合は、'/'に設定する
-// リポジトリ名を使う場合は、'/リポジトリ名/'に設定する
+// GitHub Pagesにデプロイする場合は、baseをリポジトリ名に設定
 export default defineConfig({
   plugins: [react()],
-  base: "/",
+  base: "/ai-lab/",
 });
